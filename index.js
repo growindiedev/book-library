c7e95f4d0d3f59c39db6597b0894ec75ef862706
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-08-19 23:24:16 +0530

    add script pull data form

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+var popover = document.querySelector(".popover");
+var popoverTrigger = document.querySelector(".popover__trigger");
+
+popoverTrigger.addEventListener("click", function (event) {
+  closeAllOthers(this.parentElement);
+  this.parentElement.classList.toggle("popover--active");
+});
+
+function closeAllOthers(ignore) {
+  if (popover !== ignore) {
+    popover.classList.remove("popover--active");
+  }
+}
+
+let myLibrary = [
+  { title: "The Slight Edge", author: "Jeff Olsen", pages: "500", read: true },
+];
+
+let myLibraryNodes = [];
+
+console.log("whoop", myLibrary);
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -12,14 +33,31 @@ Book.prototype.info = function () {
   return `${this.title} by ${this.author}, ${this.pages} pages, not read yet`;
 };
 
-let myLibrary = [];
+// myLibrary.map((item) => {
+//   let div = document.createElement("div");
+//   div.classList.add("grid-item");
+//   console.log("ooo", div);
+//   div.textContent = item.title;
+//   document.querySelector(".grid-container").appendChild(div);
+// });
 
-function Book() {
-  // the constructor...
+let values = [];
+function addBookToLibrary(e) {
+  e.preventDefault();
+  document.querySelectorAll("input").forEach((item) => values.push(item.value));
+  values.push(document.querySelector("input[type='checkbox']").checked);
+  myLibrary.push(new Book(...values));
+
+  myLibrary.map((item) => {
+    let div = document.createElement("div");
+    div.classList.add("grid-item");
+    console.log("ooo", div);
+    div.textContent = item.title;
+    document.querySelector(".grid-container").appendChild(div);
+  });
+  document.getElementById("myForm").reset();
 }
 
-function addBookToLibrary() {
-  // do stuff here
-}
-
-function removeBook() {}
+document
+  .querySelector("#formSent")
+  .addEventListener("click", (e) => addBookToLibrary(e));
