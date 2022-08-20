3fb3bcb3b73f0e6aa0a92ffb3bf26a4777e4fd4c
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-08-20 13:22:11 +0530

    add kitab with DOM

--- index.js
+++ index.js
@@ -44,10 +44,35 @@ Book.prototype.info = function () {
 //   document.querySelector(".grid-container").appendChild(div);
 // });
 
+function generateCard(kitab) {
+  let div = document.createElement("div");
+  div.classList.add("grid-item");
+
+  let bookName = document.createElement("p");
+  bookName.textContent = kitab.title;
+  div.appendChild(bookName);
+
+  let Author = document.createElement("p");
+  Author.textContent = kitab.author;
+  div.appendChild(Author);
+
+  let pages = document.createElement("p");
+  pages.textContent = kitab.pages;
+  div.appendChild(pages);
+
+  let readBtn = document.createElement("button");
+  readBtn.textContent = kitab.read ? "read" : "not read";
+  div.appendChild(readBtn);
+
+  let removeBtn = document.createElement("button");
+  removeBtn.textContent = "remove";
+  div.appendChild(removeBtn);
+
+  document.querySelector(".grid-container").appendChild(div);
+}
+
 let values = [];
 function addBookToLibrary(e) {
-  // container.innerHTML = "";
-
   e.preventDefault();
   document.querySelectorAll(".inp").forEach((item) => values.push(item.value));
   values.push(document.querySelector("input[type='checkbox']").checked);
@@ -55,12 +80,10 @@ function addBookToLibrary(e) {
   let kitab = new Book(...values);
   myLibrary.push(kitab);
   values = [];
+
   document.getElementById("myForm").reset();
-  let div = document.createElement("div");
-  div.classList.add("grid-item");
-  console.log("ooo", div);
-  div.textContent = kitab.title;
-  document.querySelector(".grid-container").appendChild(div);
+  generateCard(kitab);
+
   // myLibrary.map((item) => {
   //   let div = document.createElement("div");
   //   div.classList.add("grid-item");
